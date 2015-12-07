0efdd2c3a8e42e80c4c3575d6ac2fbfc36db58dc
testapp1 <testapp1@web.de>
2015-12-07 09:14:04 +0100

    Drag & drop, zoom

--- src/UI.java
+++ src/UI.java
@@ -1,11 +1,16 @@
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.*;
 import javafx.scene.control.Tooltip;
-import javafx.scene.layout.Pane;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
 import javafx.scene.shape.Cylinder;
+import javafx.scene.shape.Shape;
+import javafx.scene.shape.Shape3D;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 
 /**
@@ -13,19 +18,39 @@ import javafx.stage.Stage;
  */
 public class UI extends Application {
 
+    double originX, originY;
+
+    boolean isShiftPressed;
+
+    Camera camera;
+
+    Group root;
+
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        Pane myPane = new Pane();
+        root = new Group();
 
-        // create Scene with depth buffer
-
-        Group root = new Group();
-
+        // create scene with depth buffer
         Scene scene = new Scene(root, 600, 800, true);
 
-        Camera camera = new PerspectiveCamera(true);
+        scene.setOnMousePressed(sceneOnMousePressedEventHandler);
+        scene.setOnMouseDragged(sceneOnMouseDraggedEventHandler);
+
+        scene.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.SHIFT) {
+                isShiftPressed = true;
+            }
+        });
+
+        scene.setOnKeyReleased(event -> {
+            if (event.getCode() == KeyCode.SHIFT) {
+                isShiftPressed = false;
+            }
+        });
+
+        camera = new PerspectiveCamera(true);
 
         Box myBox1 = new Box(100, 100, 100);
         Box myBox2 = new Box(150, 20, 50);
@@ -45,32 +70,12 @@ public class UI extends Application {
         blueMaterial.setDiffuseColor(Color.DARKBLUE);
         blueMaterial.setSpecularColor(Color.BLUE);
 
-        myCylinder.setMaterial(greenMaterial);
-        myCylinder.setTranslateX(0);
-        myCylinder.setTranslateY(0);
-        myCylinder.setTranslateZ(0);
-        Tooltip.install(
-                myCylinder,
-                new Tooltip("Cylinder")
-        );
+        setShapeProperties(myCylinder, 0, 0, 0, greenMaterial, "Cylinder");
 
-        myBox1.setMaterial(blackMaterial);
-        myBox1.setTranslateX(0);
-        myBox1.setTranslateY(-100);
-        myBox1.setTranslateZ(0);
-        Tooltip.install(
-                myBox1,
-                new Tooltip("Box1")
-        );
+        setShapeProperties(myBox1, 0, -100, 0, blackMaterial, "Box1");
 
-        myBox2.setMaterial(blueMaterial);
-        myBox2.setTranslateX(0);
-        myBox2.setTranslateY(100);
-        myBox2.setTranslateZ(0);
-        Tooltip.install(
-                myBox2,
-                new Tooltip("Box2")
-        );
+        setShapeProperties(myBox2, 0, 100, 0, blueMaterial, "Box2");
+
 
 
         root.getChildren().addAll(myBox1, myBox2, myCylinder);
@@ -90,6 +95,68 @@ public class UI extends Application {
         // show scene
         primaryStage.show();
     }
+
+    /**
+     * Sets the properties of the shapes
+     * @param myShape
+     * @param x
+     * @param y
+     * @param z
+     * @param material
+     * @param toolTipText
+     * @return
+     */
+    private void setShapeProperties(Shape3D myShape, double x, double y, double z, PhongMaterial material, String toolTipText ){
+        myShape.setMaterial(material);
+        myShape.setTranslateX(x);
+        myShape.setTranslateY(y);
+        myShape.setTranslateZ(z);
+        Tooltip.install(
+                myShape,
+                new Tooltip(toolTipText)
+        );
+    }
+
+
+    /**
+     * Eventhandler for mouse pressed for circle drag
+     */
+    EventHandler<MouseEvent> sceneOnMousePressedEventHandler =
+            new EventHandler<MouseEvent>() {
+
+                @Override
+                public void handle(MouseEvent t) {
+                    // set origin coordinates
+                    originX = t.getSceneX();
+                    originY = t.getSceneY();
+                }
+            };
+
+    /**
+     * Eventhandler for mouse follow on drag
+     */
+    EventHandler<MouseEvent> sceneOnMouseDraggedEventHandler =
+            new EventHandler<MouseEvent>() {
+
+                @Override
+                public void handle(MouseEvent t) {
+                    // calculate offset
+                    double offsetX = t.getSceneX() - originX;
+                    double offsetY = t.getSceneY() - originY;
+                    // follow mouse
+                    // move left and right
+                    if (isShiftPressed) {
+                        camera.setTranslateZ(camera.getTranslateZ() + (offsetX + offsetY) / 2);
+                    } else {
+                        root.getTransforms().add(new Rotate(offsetX, 0, 0, 0, Rotate.Y_AXIS));
+                        root.getTransforms().add(new Rotate(offsetY, 0, 0, 0, Rotate.Z_AXIS));
+                    }
+
+                    originX += offsetX;
+                    originY += offsetY;
+
+                }
+            };
 
 
 }
